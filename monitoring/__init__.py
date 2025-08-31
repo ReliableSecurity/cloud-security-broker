767c3b9a09970ea1ac25a2c70901bdf05c9e46ff
ReliableSecurity <122018430+ReliableSecurity@users.noreply.github.com>
2025-08-31 21:39:24 +0300

    Add monitoring module init

---
+++ monitoring/__init__.py
@@ -0,0 +1,8 @@
+"""
+Модуль мониторинга CASB системы
+Отслеживание активности в облачных сервисах и обнаружение угроз
+"""
+
+from .cloud_monitor import CloudMonitoring
+
+__all__ = ['CloudMonitoring']
