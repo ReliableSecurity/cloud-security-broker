b6652c8484e5828123bcfdbb51b883d69f40dfe9
ReliableSecurity <122018430+ReliableSecurity@users.noreply.github.com>
2025-08-31 21:39:40 +0300

    Add policies module init

---
+++ policies/__init__.py
@@ -0,0 +1,8 @@
+"""
+Модуль политик безопасности CASB системы
+Управление и применение политик безопасности
+"""
+
+from .policy_engine import SecurityPolicyManager
+
+__all__ = ['SecurityPolicyManager']
